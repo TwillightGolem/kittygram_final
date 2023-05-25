3cce152741d524ad27c792892e6813a3fb2a0c56
TwillightSparkl <twillightsparkl@gmail.com>
2023-05-25 11:06:10 +0300

    Sprint15_Kitty_1

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['158.160.2.96', '127.0.0.1', 'localhost', 'kittygm.sytes.net']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -116,3 +116,5 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
 
 }
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
